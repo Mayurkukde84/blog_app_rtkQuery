7d9973ab667e374fbfe2ad641d83d71fa4cbc9bf
mayur <mayurkukde84@gmail.com>
2023-01-29 13:31:43 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
-app.listen(5000,()=>console.log(`server is running on port 5000`))
+PORT = process.env.PORT
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
